325fdc9e3f0b37c77b40df925788e85aefd61bee
Albert Julius Liu <ajul1987@gmail.com>
2021-05-08 17:49:40 -0700

    parameterize tests

--- tests/pure_dice_pool_test.py
+++ tests/pure_dice_pool_test.py
@@ -4,6 +4,8 @@ from hdroller import Die, PureDicePool
 import numpy
 import pytest
 
+abs_tol = 1e-9
+
 class PureDicePoolBruteForce():
     def __init__(self, num_dice, single_die):
         self._num_dice = num_dice
@@ -22,8 +24,8 @@ class PureDicePoolBruteForce():
         pmf = numpy.zeros((pmf_length,))
         for rolls in numpy.ndindex((self.single_len(),) * self._num_dice):
             total = sum(sorted(rolls)[-num_keep:])
-            pmf[total] += 1.0
-        pmf /= numpy.power(self.single_len(), self._num_dice)
+            mass = numpy.product(self._single_die.pmf()[numpy.array(rolls)])
+            pmf[total] += mass
         return Die(pmf, min_outcome)
 
     def keep_index(self, index):
@@ -31,30 +33,46 @@ class PureDicePoolBruteForce():
         min_outcome = self.single_min_outcome()
         for rolls in numpy.ndindex((self.single_len(),) * self._num_dice):
             picked = sorted(rolls)[index]
-            pmf[picked] += 1.0
-        pmf /= numpy.power(self.single_len(), self._num_dice)
+            mass = numpy.product(self._single_die.pmf()[numpy.array(rolls)])
+            pmf[picked] += mass
         return Die(pmf, min_outcome)
 
-def test_keep_highest_of_7d6():
-    for num_keep in range(8):
-        pool = PureDicePool(7, Die.d6)
-        pool_result = pool.keep_highest(num_keep)
+@pytest.mark.parametrize('num_keep', range(8))
+def test_keep_highest_of_7d6(num_keep):
+    num_dice = 7
+    single_die = Die.d6
 
-        brute_pool = PureDicePoolBruteForce(7, Die.d6)
-        brute_pool_result = brute_pool.keep_highest(num_keep)
-        
-        print(pool_result)
-        print(brute_pool_result)
+    pool = PureDicePool(num_dice, single_die)
+    pool_result = pool.keep_highest(num_keep)
 
-        assert pool_result.ks_stat(brute_pool_result) == pytest.approx(0.0)
+    brute_pool = PureDicePoolBruteForce(num_dice, single_die)
+    brute_pool_result = brute_pool.keep_highest(num_keep)
 
-def test_keep_index_of_7d6():
-    for index in range(7):
-        pool = PureDicePool(7, Die.d6)
-        pool_result = pool.keep_index(index)
+    assert pool_result.ks_stat(brute_pool_result) == pytest.approx(0.0, abs=abs_tol)
 
-        brute_pool = PureDicePoolBruteForce(7, Die.d6)
-        brute_pool_result = brute_pool.keep_index(index)
+@pytest.mark.parametrize('num_keep', range(9))
+def test_keep_highest_of_8_weighted(num_keep):
+    num_dice = 8
+    single_die = Die.from_faces([1, 1, 1, 2, 2, 2, 2, 3])
 
-        assert pool_result.ks_stat(brute_pool_result) == pytest.approx(0.0)
+    pool = PureDicePool(num_dice, single_die)
+    pool_result = pool.keep_highest(num_keep)
+
+    brute_pool = PureDicePoolBruteForce(num_dice, single_die)
+    brute_pool_result = brute_pool.keep_highest(num_keep)
+
+    assert pool_result.ks_stat(brute_pool_result) == pytest.approx(0.0, abs=abs_tol)
+
+@pytest.mark.parametrize('index', range(7))
+def test_keep_index_of_7d6(index):
+    num_dice = 7
+    single_die = Die.d6
+    
+    pool = PureDicePool(num_dice, single_die)
+    pool_result = pool.keep_index(index)
+
+    brute_pool = PureDicePoolBruteForce(num_dice, single_die)
+    brute_pool_result = brute_pool.keep_index(index)
+
+    assert pool_result.ks_stat(brute_pool_result) == pytest.approx(0.0, abs=abs_tol)
         
