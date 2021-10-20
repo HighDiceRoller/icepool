c047f54d2db39830b783f1c046bbad2a4ca1bd37
Albert Julius Liu <ajul1987@gmail.com>
2021-10-19 23:25:32 -0700

    better canonical args

--- src/hdroller/countdown.py
+++ src/hdroller/countdown.py
@@ -7,28 +7,47 @@ import hdroller.math
 
 import numpy
 
-def _countdown(die_sizes, keep_mask, weights=None):
+def countdown(keep_mask, die=None, die_sizes=None):
     """
-    die_sizes: an array of integers, one per die, denoting how many faces that die has.
-    keep: Specifies which indexes of dice to keep. Should be of the same size as die_sizes.
-    weights: an array of size at least equal to the largest of die_sizes, denoting the weight of each face.
-        If omitted, all faces will be given a weight of 1.
+    keep_mask: A mask of one boolean per die from lowest to highest, denoting whether to keep that sorted die.
+    die: A reference Die which weights each possible outcome of each individual die.
+        If omitted, outcomes will be 1 ... max(die_sizes) inclusive with a weight of 1 per outcome.
+    die_sizes: An array of integers, one per die.
+        Individual dice will have faces equal to the first die_size faces of the reference die.
+        If omitted, every die will have size equal to the reference die.
     """
+    
+    if die is None and die_sizes is None:
+        raise ValueError('At least one of die and die_sizes must be provided.')
+    
+    num_keep = numpy.count_nonzero(keep_mask)
+    if num_keep == 0:
+        return Die(0)
     
     keep_mask_desc = numpy.flip(keep_mask)
-    num_keep = numpy.count_nonzero(keep_mask)
-    num_dice = len(die_sizes)
+    num_dice = len(keep_mask)
     
-    die_sizes_desc = numpy.flip(numpy.sort(die_sizes))
+    if die_sizes is None:
+        die_sizes = numpy.array([len(die)] * num_dice)
+        die_sizes_desc = die_sizes
+    else:
+        die_sizes_desc = numpy.flip(numpy.sort(die_sizes))
+        
     max_face = die_sizes_desc[0] - 1
     max_totals = numpy.insert(numpy.cumsum((die_sizes_desc-1) * keep_mask_desc), 0, 0.0)
-    if weights is None:
-        weights = numpy.ones((max_face + 1,))
+    
+    if die is None:
+        die = hdroller.Die.standard(max_face + 1)
+    
+    weights = die.weights()
+    
     if not hdroller.math.product_of_total_weights_is_exact(weights[:die_size] for die_size in die_sizes):
         weights = weights / numpy.cumsum(weights)[-1]
+        
+    # TODO: terminate early if there are no more dice to keep.
     weights_below = numpy.insert(numpy.cumsum(weights), 0, 0.0)
         
-    # (number of consumed dice, running total) -> weight of having this running total. Denominator is equal to product of weights of the remaining dice and sizes.
+    # (number of consumed dice, running sum) -> number of ways of rolling this running sum with all consumed dice greater than curr_face.
     state = numpy.zeros((num_dice + 1, max_totals[-1] + 1))
     state[0, 0] = 1.0
     
@@ -38,7 +57,6 @@ def _countdown(die_sizes, keep_mask, weights=None):
         # The total number of dice that are large enough to roll the current face.
         curr_num_dice = numpy.count_nonzero(die_sizes > curr_face)
         weight = weights[curr_face]
-        weight_below = weights_below[curr_face]
         for prev_num_consumed_dice in range(0, prev_num_dice+1):
             num_active_dice = curr_num_dice - prev_num_consumed_dice
             prev_max_total = max_totals[prev_num_consumed_dice]
@@ -51,38 +69,6 @@ def _countdown(die_sizes, keep_mask, weights=None):
         state = next_state
         prev_num_dice = curr_num_dice
     
-    return state[-1, :]
-    
-    """
-    for each stage:
-        for each number of remaining dice:
-            for each number of dice rolling within range:
-                compute convolution of sum
-                compute chance of rolling within range
-                    normalized: binom * chance of rolling within range ** number of dice rolled within range * (1 - chance of roling within range) ** number of eligible dice not rolled within range
-                    non-normalized: chance of rolling within range = weights within range / remaining weight, so denominator must be multiple of remaining weight ** number of eligible dice?
-                update state transition
-                
-                example: d5, d5, d3, d3, d2
-                first stage:
-                    state 0, ...: weight of both d5s rolling 3 or less, total weight = 9 * 18 / 450
-                    state 1, ...: weight of one d5 rolling 3 or less, total weight = 12 * 18 / 450
-                    state 2, ...: weight of both d5s rolling 4-5, total weight = 4 * 18 / 450
-                second stage:
-                    state 0: we have 4x d3s -> denominator of 81 * 2.
-                    state 1: we have 3x d3s -> denominator of 27 * 2.
-                    state 2: we have 2x d3s -> denominator of 9 * 2.
-                    
-                    
-                what does state represent? conditioned on this many dice used/remaining, weight of running total? final denominator = remaining weight ** number of eligible dice?
-    """
-
-def _keep_pool(die, num_dice, keep_mask, die_sizes=None):
-    if die_sizes is None:
-        die_sizes = numpy.array([len(die)] * num_dice)
-    min_outcome = numpy.count_nonzero(keep_mask) * die.min_outcome()
-    return hdroller.Die(_countdown(die_sizes, keep_mask, weights=die.weights()), min_outcome)
-
-def _keep_standard(die_sizes, keep_mask):
-    min_outcome = numpy.count_nonzero(keep_mask)
-    return hdroller.Die(_countdown(die_sizes, keep_mask), min_outcome)
+    result_min_outcome = num_keep * die.min_outcome()
+    result_weights = state[-1, :]
+    return hdroller.Die(result_weights, result_min_outcome)
