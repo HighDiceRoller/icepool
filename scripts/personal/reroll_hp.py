bf29511648d3e70e3349345f4c934774abc493bb
Albert Julius Liu <ajul1987@gmail.com>
2021-11-07 13:54:00 -0800

    script for computing option for rerolling all hit dice at each level

---
+++ scripts/personal/reroll_hp.py
@@ -0,0 +1,30 @@
+import _context
+
+import functools
+
+from hdroller import Die
+import numpy
+
+def opt_hp_at_max_level(die, max_level):
+    """
+    current_hp: A Die representing the distribution of current hit points.
+    """
+    @functools.lru_cache(maxsize=None)
+    def inner(current_level, current_hp):
+        if current_level == max_level:
+            return current_hp
+        hp_if_no_reroll = 0.0
+        for outcome, p in zip(die.outcomes(), die.pmf()):
+            hp_if_no_reroll += inner(current_level + 1, current_hp + outcome) * p
+        hp_if_reroll = 0.0
+        reroll_die = (current_level + 1) * die
+        for outcome, p in zip(reroll_die.outcomes(), reroll_die.pmf()):
+            hp_if_reroll += inner(current_level + 1, outcome) * p
+
+        return max(hp_if_no_reroll, hp_if_reroll)
+
+    return inner(1, die.max_outcome())
+
+dice = [Die.d4, Die.d6, Die.d8, Die.d10, Die.d12]
+for die in dice:
+    print(opt_hp_at_max_level(die, 19) + die.max_outcome())
