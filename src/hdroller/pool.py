3f43fdc72987be46fa2362fcff358eea110bb775
Albert Julius Liu <ajul1987@gmail.com>
2022-01-21 21:54:47 -0800

    remove old evaluate code, use variadic arguments for state

--- src/hdroller/pool.py
+++ src/hdroller/pool.py
@@ -10,6 +10,8 @@ Possible todo:
 * Specialist algorithms for add, max, keep-highest.
 * Auto state shape, min/max score?
 * Early termination?
+* Cacheable next_state?
+* Use cache module?
 
 Possible restrictions:
 * Max score is achieved by rolling max on all dice?
@@ -25,13 +27,18 @@ Systems:
 * Neon City Overdrive: multiple pools
 """
 
+def _tuplelize(x):
+    if not isinstance(x, tuple):
+        x = (x,)
+    return x
+
 class PoolScorer():
     def initial_state(self, die, max_outcomes):
         """
         Arguments:
             max_outcomes: The maximum outcome on each die, in descending order.
         Returns:
-            A hashable object indicating the initial state.
+            A hashable indicating the initial state.
         
         Note that if you want to keep track of the total number/size of dice
         or the number of dice consumed so far, you will need to insert
@@ -41,29 +48,39 @@ class PoolScorer():
         """
         raise NotImplementedError()
 
-    def next_state(self, state, outcome, count):
+    def next_state(self, outcome, count, *state):
         """
         Arguments:
-            state: A hashable object indicating the current state.
             outcome: The current outcome.
             count: How many dice rolled this outcome.
+            *state: A hashable indicating the current state.
+                Subclasses may implement this as one or more ordered arguments
+                rather than as a variadic argument.
         
         Returns:
             A hashable object indicating the next state resulting from the arguments.
         """
         raise NotImplementedError()
         
-    def score(self, state, die, max_outcomes):
+    def score(self, die, max_outcomes, *state):
         """
         Arguments:
-            state: A final state after all dice in the pool have been consumed.
+            *state: A final state after all dice in the pool have been consumed.
+                Subclasses may implement this as one or more ordered arguments
+                rather than as a variadic argument.
         
         Returns:
             An integer indicating the final score for that state.
         """
         raise NotImplementedError()
+   
+    def _initial_state_tuple(self, die, max_outcomes):
+        return _tuplelize(self.initial_state(die, max_outcomes))
         
-    def evaluate_state_recursive(self, die, outcome, max_outcomes, initial_state):
+    def _next_state_tuple(self, outcome, count, *state):
+        return _tuplelize(self.next_state(outcome, count, *state))
+        
+    def _evaluate_internal(self, die, outcome, max_outcomes, initial_state):
         """
         Arguments:
             outcome: The current outcome under consideration. Decreases by 1 each recursion.
@@ -73,6 +90,9 @@ class PoolScorer():
         Returns:
             A dictionary { state : weight } describing the probability distribution over states.
         """
+        cache_key = (die, outcome, max_outcomes, initial_state)
+        if cache_key in self._cache: return self._cache[cache_key]
+        
         result = defaultdict(float)
         num_dice = len(max_outcomes)
         index = outcome - die.min_outcome()
@@ -80,7 +100,7 @@ class PoolScorer():
         if index == 0:
             # All dice roll the only possible value.
             count = len(max_outcomes)
-            state = self.next_state(initial_state, outcome, count)
+            state = self._next_state_tuple(outcome, count, *initial_state)
             weight = single_weight ** count
             result[state] = weight
         else:
@@ -89,62 +109,42 @@ class PoolScorer():
                 num_eligible_dice += 1
             tail_max_outcomes = (outcome - 1,) * num_eligible_dice + max_outcomes[num_eligible_dice:]
             
-            binomial_weights = hdroller.math.binomial_row(num_eligible_dice, single_weight)
-            for count, binomial_weight in enumerate(binomial_weights):
-                tail = self.evaluate_state_recursive(die, outcome - 1, tail_max_outcomes[count:], initial_state)
+            if single_weight > 0.0:
+                binomial_weights = hdroller.math.binomial_row(num_eligible_dice, single_weight)
+                for count, binomial_weight in enumerate(binomial_weights):
+                    tail = self._evaluate_internal(die, outcome - 1, tail_max_outcomes[count:], initial_state)
+                    for state, tail_weight in tail.items():
+                        next_state = self._next_state_tuple(outcome, count, *state)
+                        result[next_state] += tail_weight * binomial_weight
+            else:
+                # If this outcome has zero weight, zero dice can roll this outcome.
+                tail = self._evaluate_internal(die, outcome - 1, tail_max_outcomes, initial_state)
                 for state, tail_weight in tail.items():
-                    next_state = self.next_state(state, outcome, count)
+                    next_state = self._next_state_tuple(outcome, 0, *state)
                     result[next_state] += tail_weight * binomial_weight
+
+        self._cache[cache_key] = result
         return result
     
-    def evaluate_recursive(self, die, pool):
+    def evaluate(self, die, pool):
         """
         pool: Either an integer indicating the size of the pool,
             or an iterable of integers, each denoting the maximum outcome on one die in the pool.
         """
+        if not hasattr(self, '_cache'):
+            self._cache = {}
+        
         if numpy.issubdtype(type(pool), numpy.integer):
             max_outcomes = (die.max_outcome(),) * pool
         else:
             max_outcomes = tuple(reversed(sorted(pool)))
-        initial_state = self.initial_state(die, max_outcomes)
+        initial_state = self._initial_state_tuple(die, max_outcomes)
         
-        dist = self.evaluate_state_recursive(die, die.max_outcome(), max_outcomes, initial_state)
+        dist = self._evaluate_internal(die, die.max_outcome(), max_outcomes, initial_state)
         
         score_dist = defaultdict(float)
         for state, weight in dist.items():
-            score = self.score(state, die, max_outcomes)
+            score = self.score(die, max_outcomes, *state)
             score_dist[score] += weight
         
         return hdroller.Die(score_dist)
-        
-    def evaluate(self, die, num_dice):
-        """
-        Arguments:
-            die: The die to evaluate.
-            num_dice: The number of dice in the pool.
-                
-        Returns:
-            A Die representing the final score.
-        """
-        
-        prev_dist = defaultdict(float)
-        prev_dist[(0, self.initial_state(num_dice))] = 1.0
-        
-        for outcome, outcome_weight in reversed(list(zip(die.outcomes(), die.weights()))):
-            dist = defaultdict(float)
-            for (prev_consumed_dice, prev_state), prev_weight in prev_dist.items():
-                remaining_dice = num_dice - prev_consumed_dice
-                next_weights = prev_weight * hdroller.math.binomial_row(remaining_dice, outcome_weight)
-                for count, next_weight in enumerate(next_weights):
-                    next_state = self.next_state(prev_state, outcome, count)
-                    next_indexes = (prev_consumed_dice + count, next_state)
-                    dist[next_indexes] += next_weight
-            prev_dist = dist
-        
-        final_scores = defaultdict(float)
-        for (prev_consumed_dice, prev_state), prev_weight in prev_dist.items():
-            if prev_consumed_dice == num_dice:
-                score = self.score(prev_state, num_dice)
-                final_scores[score] += prev_weight
-        
-        return hdroller.Die(final_scores)
