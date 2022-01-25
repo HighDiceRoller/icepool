a90c3f331aaad34c74df36e00e7c971213350acf
Albert Julius Liu <ajul1987@gmail.com>
2022-01-24 21:29:51 -0800

    remove now-unused math functions

--- src/hdroller/math.py
+++ src/hdroller/math.py
@@ -1,28 +1,7 @@
 import numpy
-import math
 from functools import lru_cache
 
 MAX_INT_FLOAT = 2 ** 53
-
-factorial = math.factorial
-
-def comb(n, k, exact=True, repetition=False):
-    """
-    Same signature as scipy.comb with exact = True; only accepts integers.
-    """
-    if repetition:
-        return math.comb(n + k - 1, k)
-    else:
-        return math.comb(n, k)
-
-def multinom(n, ks):
-    """
-    Multinomial coefficient.
-    """
-    if len(ks) == 0:
-        return 1
-    else:
-        return comb(n, ks[0]) * multinom(n-ks[0], ks[1:])
 
 def reverse_cumsum(a, axis=None, *args, **kwargs):
     """
@@ -30,27 +9,6 @@ def reverse_cumsum(a, axis=None, *args, **kwargs):
     Any additional arguments are sent to cumsum.
     """
     return numpy.flip(numpy.cumsum(numpy.flip(a, axis=axis), axis=axis, *args, **kwargs), axis=axis)
-    
-def reverse_cumprod(a, axis=None, *args, **kwargs):
-    """
-    Applies a cumsum in reverse direction along the specified axis.
-    Any additional arguments are sent to cumsum.
-    """
-    return numpy.flip(numpy.cumprod(numpy.flip(a, axis=axis), axis=axis, *args, **kwargs), axis=axis)
-
-def convolve_along_last_axis(a, v):
-    if a.shape[-1] < v.shape[-1]:
-        a, v = v, a
-    conv_count = numpy.prod(a.shape[:-1])
-    conv_length = a.shape[-1] + v.shape[-1] - 1
-    new_shape = a.shape[:-1] + (conv_length,)
-    result = numpy.zeros(new_shape)
-    
-    # This seems to be almost always faster than convolving each slice.
-    for i in range(v.shape[-1]):
-        result[..., i:i+a.shape[-1]] += a * v[..., i:i+1]
-    
-    return result
 
 @lru_cache(maxsize=None)
 def binom_row(n, b):
@@ -64,11 +22,3 @@ def binom_row(n, b):
         result = numpy.append(prev, 0.0) + numpy.insert(b * prev, 0, 0.0)
     result.setflags(write=False)
     return result
-
-def should_normalize_weight_product(weight_arrays):
-    total_prod = 1.0
-    for weights in weight_arrays:
-        total_prod *= numpy.cumsum(weights)[-1]
-        if total_prod >= MAX_INT_FLOAT:
-            return True
-    return False
