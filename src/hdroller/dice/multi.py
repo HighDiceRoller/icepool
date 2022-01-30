ff6d43597bffe29ddf362f5d9a2b2ed9736727ab
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-01-30 00:04:31 -0800

    compute ndims properly in MultiDie.__getitem__()

--- src/hdroller/dice/multi.py
+++ src/hdroller/dice/multi.py
@@ -12,20 +12,20 @@ class MultiDie(hdroller.dice.base.BaseDie):
     Outcomes are tuples, and operations are performed on each element of the tuples.
     """
     
-    def unary_op(self, op, *args):
+    def unary_op(self, op, *args, **kwargs):
         """Returns a die representing the effect of performing the operation on the outcomes."""
         data = defaultdict(int)
         for outcome, weight in self.items():
-            new_outcome = tuple(op(x, *args) for x in outcome)
+            new_outcome = tuple(op(x, *args, **kwargs) for x in outcome)
             data[new_outcome] += weight
         return hdroller.die(data, ndim=self.ndim())
     
-    def binary_op(self, other, op, *args):
+    def binary_op(self, other, op, *args, **kwargs):
         """Returns a die representing the effect of performing the operation on pairs of outcomes from the two dice."""
         ndim = self.common_ndim(other)
         data = defaultdict(int)
         for (outcome_self, weight_self), (outcome_other, weight_other) in itertools.product(self.items(), other.items()):
-            new_outcome = tuple(op(x, y, *args) for x, y in zip(outcome_self, outcome_other))
+            new_outcome = tuple(op(x, y, *args, **kwargs) for x, y in zip(outcome_self, outcome_other))
             data[new_outcome] += weight_self * weight_other
         return hdroller.die(data, ndim=ndim)
     
@@ -35,7 +35,11 @@ class MultiDie(hdroller.dice.base.BaseDie):
     
     def __getitem__(self, select):
         """Slices the outcomes of the die."""
-        return hdroller.dice.single.SingleDie.unary_op(self, self._getitem, select)
+        data = defaultdict(int)
+        for outcome, weight in self.items():
+            data[hdroller.indexing.select_from(outcome, select)] = weight
+        ndim = sum(hdroller.indexing.select_bools(self.ndim(), select))
+        return hdroller.die(data, ndim=ndim)
     
     # Statistics.
     
