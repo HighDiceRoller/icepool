bc67f656c7fa09587785ad80690ff39f99843ef1
Albert Julius Liu <ajul1987@gmail.com>
2022-01-29 22:44:05 -0800

    rename create -> func since not all functions strictly deal with creation

--- src/hdroller/__init__.py
+++ src/hdroller/__init__.py
@@ -1,7 +1,7 @@
-from hdroller.dice.create import *
-from hdroller.dice.create import __getattr__
+from hdroller.dice.func import *
+from hdroller.dice.func import __getattr__
 from hdroller.dice_pool import pool
-from hdroller.pool_eval import PoolEval, PoolSum
+from hdroller.pool_eval import PoolEval, pool_sum
 from hdroller.dice.base import _align
 
 min = hdroller.dice.base.BaseDie.min
