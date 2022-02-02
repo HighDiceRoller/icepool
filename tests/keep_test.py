7b60773dcc047294406eccd065987d29bee34b30
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-02-01 23:15:34 -0800

    add keep_test changes from last commit message

--- tests/keep_test.py
+++ tests/keep_test.py
@@ -23,28 +23,28 @@ def bf_keep(die, num_dice, keep_indexes):
         return sorted(outcomes)[keep_indexes]
     return hdroller.apply(func, *([die] * num_dice))
 
-@pytest.mark.parametrize('num_keep', range(1, 4))
+@pytest.mark.parametrize('num_keep', range(1, 6))
 def test_keep_highest(num_keep):
     die = hdroller.d12
     result = die.keep_highest(4, num_keep)
     expected = bf_keep_highest(die, 4, num_keep)
     assert result == expected
 
-@pytest.mark.parametrize('num_keep', range(1, 4))
+@pytest.mark.parametrize('num_keep', range(1, 6))
 def test_keep_highest_mixed_drop_highest(num_keep):
     die = hdroller.d12
     result = die.keep_highest(4, num_keep, num_drop=1)
     expected = bf_keep_highest(die, 4, num_keep, num_drop=1)
     assert result == expected
 
-@pytest.mark.parametrize('num_keep', range(1, 4))
+@pytest.mark.parametrize('num_keep', range(1, 6))
 def test_keep_lowest(num_keep):
     die = hdroller.d12
     result = die.keep_lowest(4, num_keep)
     expected = bf_keep_lowest(die, 4, num_keep)
     assert result == expected
 
-@pytest.mark.parametrize('num_keep', range(1, 4))
+@pytest.mark.parametrize('num_keep', range(1, 6))
 def test_keep_lowest_mixed_drop_highest(num_keep):
     die = hdroller.d12
     result = die.keep_lowest(4, num_keep, num_drop=1)
@@ -60,6 +60,6 @@ def test_keep_index(keep_index):
 
 def test_max_outcomes():
     die = hdroller.d12
-    result = die.keep(2, max_outcomes=[8, 6])
+    result = die.keep(max_outcomes=[8, 6])
     expected = hdroller.d8 + hdroller.d6
     assert result == expected
