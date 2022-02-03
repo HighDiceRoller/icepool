7eeabc1b5234e41591f78fc3ce008c9624fe3bcc
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-02-02 21:43:02 -0800

    improve pool_eval docstrings

--- src/hdroller/pool_eval.py
+++ src/hdroller/pool_eval.py
@@ -11,8 +11,6 @@ import math
 class PoolEval(ABC):
     """ An abstract class for evaulating one or more `DicePool`s.
     
-    Instances cache all intermediate state distributions.
-    
     You can pretend that evaluation occurs on individual rolls of the pool as follows:
     
     1. First, specify the `initial_state()`. The state is usually some sort of running total.
@@ -22,14 +20,17 @@ class PoolEval(ABC):
     3. Finally and optionally, once all outcomes have been accounted for,
         use `final_outcome()` to specify the result of that roll.
     
-    After construction, subclasses should not modify `self`,
-    nor allow `self` to be modified externally.
+    Instances cache all intermediate state distributions.
+    You should therefore reuse instances when possible.
+    
+    Instances should not be modified after construction
+    in any way that affects the return values of these methods.
+    Otherwise, values in the cache may be incorrect.
     """
+    
     @abstractmethod
     def initial_state(self, *pools):
         """ Generates an initial state for the evaluation.
-        
-        This should be a pure function with no side-effects.
         
         Arguments:
             *pools: One or more `DicePool`s being evaluated.
@@ -39,7 +40,8 @@ class PoolEval(ABC):
         
         If you want to use information about the pool(s) being evaluated,
         you need to save that information to the state here.
-        This is to avoid polluting cache keys with unused information.
+        This is to avoid polluting the cache with duplicate entries
+        based on irrelevant information in the key.
         """
     
     @abstractmethod
@@ -49,12 +51,12 @@ class PoolEval(ABC):
         This should produce a state given the previous state, an outcome, and the number of dice in each pool rolling that outcome.
         Note that outcomes are not guaranteed to be consecutive, as outcomes not present in any pool will not be considered.
         
-        This should be a pure function with no side-effects.
-        
         Args:
             prev_state: A hashable object indicating the state before rolling the current outcome.
             outcome: The current outcome.
             counts: One `int`for each pool indicating how many dice in that pool rolled the current outcome.
+                If the outcome does not appear in the fundamental die of the pool, this is `None`.
+                Zero-weight outcomes count as appearing.
         
         Returns:
             A hashable object indicating the next state.
@@ -66,8 +68,6 @@ class PoolEval(ABC):
         """ Optional function to generate a final outcome from a final state.
         
         If not overridden, the final state is used as the final outcome.
-        
-        This should be a pure function with no side-effects.
         
         Args:
             final_state: A state after all outcomes have been processed.
@@ -134,6 +134,7 @@ class PoolEval(ABC):
             initial_state: The initial state. This is passed without change recursively.
             *pools: One or more `DicePool`s to evaluate.
                 Values of `None` indicate that pool has no remaining outcomes to process.
+                This *does* change recursively.
             
         Returns:
             A dict `{ state : weight }` describing the probability distribution over states.
