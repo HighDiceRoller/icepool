c8b2bccbd337eb699aa0d2866ad661c8081fa272
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-02-03 00:25:11 -0800

    add missing functools.py

---
+++ src/hdroller/functools.py
@@ -0,0 +1,19 @@
+import functools
+
+def die_cache(func):
+    """ Workaround for `BaseDie.__eq__()` not being usable for hashing.
+    
+    Uses the `BaseDie`'s alternative hash key to create something hashable.
+    """
+    cache = {}
+    @functools.wraps(func)
+    def wrapped(die, *args, **kwargs):
+        key = (die.key_tuple(),) + args
+        if key in cache:
+            return cache[key]
+        else:
+            result = func(die, *args, **kwargs)
+            cache[key] = result
+            return result
+    
+    return wrapped
