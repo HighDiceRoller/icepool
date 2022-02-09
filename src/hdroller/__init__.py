b7b658ec409e7f1d0842d79c4504e2e811eb10d2
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-02-08 23:41:38 -0800

    minor reordering of __all__

--- src/hdroller/__init__.py
+++ src/hdroller/__init__.py
@@ -14,8 +14,8 @@ from hdroller.die.vector import VectorDie
 
 highest = hdroller.die.base.BaseDie.highest
 lowest = hdroller.die.base.BaseDie.lowest
-min_outcome = hdroller.die.base.BaseDie.min_outcome
 max_outcome = hdroller.die.base.BaseDie.max_outcome
+min_outcome = hdroller.die.base.BaseDie.min_outcome
 
 from hdroller.pool import Pool, DicePool
 from hdroller.eval_pool import EvalPool, SumPool, sum_pool, FindMatchingSets
@@ -23,7 +23,7 @@ from hdroller.eval_pool import EvalPool, SumPool, sum_pool, FindMatchingSets
 __all__ = ['Die', 'standard', 'd', '__getattr__', 'bernoulli', 'coin',
     'BaseDie', 'ScalarDie', 'VectorDie',
     'from_cweights', 'from_sweights', 'from_rv', 'mix', 'if_else', 'align', 'align_range', 'check_ndim',
-    'lowest', 'highest', 'min_outcome', 'max_outcome',
+    'lowest', 'highest', 'max_outcome', 'min_outcome',
     'apply',
     'Pool', 'DicePool', 'EvalPool', 'SumPool', 'sum_pool', 'FindMatchingSets',
     'd2', 'd3', 'd4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']
