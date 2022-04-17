1f91c9fc803a6310ec8f29d2d68d4e6506b95d6a
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-04-17 15:28:24 -0700

    move pool code to subpackage

--- src/icepool/__init__.py
+++ src/icepool/__init__.py
@@ -24,8 +24,8 @@ lowest = icepool.die.base.BaseDie.lowest
 max_outcome = icepool.die.base.BaseDie.max_outcome
 min_outcome = icepool.die.base.BaseDie.min_outcome
 
-from icepool.pool import Pool, standard_pool, DicePool
-from icepool.eval_pool import EvalPool, WrapFuncEval, SumPool, sum_pool, FindBestSet, FindBestRun
+from icepool.pool.pool import Pool, standard_pool, DicePool
+from icepool.pool.eval import EvalPool, WrapFuncEval, SumPool, sum_pool, FindBestSet, FindBestRun
 
 import enum
 
