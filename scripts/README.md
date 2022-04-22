62e3edd2e80784b1604137285158c51b8ec00bc2
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-04-21 20:34:03 -0700

    remove scripts

--- scripts/README.md
+++
@@ -1,1 +0,0 @@
-The /personal subdirectory contains my personal scripts. Most of them probably don't work as the API has moved on, so don't take them as good examples of anything.
--- scripts/personal/nwod.py
+++
@@ -1,8 +0,0 @@
-import _context
-
-from icepool import Die
-import numpy
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-nwod = Die.mix([0]*7 + [1]*3).explode(6, chance=0.1)
