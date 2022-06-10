95188acde75dde4fa162c9f31fe5eac605337222
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-06-09 23:49:33 -0700

    typing for math.py

--- src/icepool/math.py
+++ src/icepool/math.py
@@ -4,15 +4,14 @@ from functools import cache
 
 
 @cache
-def comb_row(n, b):
+def comb_row(n: int, b: int) -> tuple[int, ...]:
     """Returns a tuple of n+1 elements, where the kth element is equal to math.comb(n, k) * b ** k.
 
     The results are cached.
     """
     if n == 0:
-        result = (1,)
+        return (1,)
     else:
         prev = comb_row(n - 1, b)
-        result = (1,) + tuple(
+        return (1,) + tuple(
             x + b * y for x, y in zip(prev[1:], prev[:-1])) + (b * prev[-1],)
-    return result
