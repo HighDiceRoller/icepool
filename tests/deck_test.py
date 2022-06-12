ff155fcb10edf127c6d53d49d468661a8b64053b
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-06-11 23:57:41 -0700

    first deck test

---
+++ tests/deck_test.py
@@ -0,0 +1,14 @@
+import _context
+
+import icepool
+import pytest
+
+from icepool import FindBestRun
+
+# no wraparound
+find_best_run = FindBestRun()
+
+def test_poker_straight():
+    deck = icepool.CardDraw(*range(13), dups=[4]*13, hand_size=5)
+    result = find_best_run.eval(deck)[0] == 5
+    assert result.equals(icepool.coin(9216, 2598960))
