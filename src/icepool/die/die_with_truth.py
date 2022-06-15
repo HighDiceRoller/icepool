603118504cd764513665efe1f3e9ad74600f000c
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-06-15 00:27:02 -0700

    Revert "warn if both Die and truth value outputs of a comparator are used"
    This reverts commit 9f6b92bf6c0a72df6f4a706bb621d15d3d8b42bc.

--- src/icepool/die/die_with_truth.py
+++ src/icepool/die/die_with_truth.py
@@ -5,7 +5,6 @@ from icepool.counts import Counts
 from icepool.die.die import Die
 
 from functools import cached_property
-import warnings
 
 from typing import Callable
 
@@ -16,8 +15,6 @@ class DieWithTruth(Die):
     Additionally, the data is evaluated lazily since the caller may only be
     interested in the truth value.
     """
-    _used_callback: bool
-    """Whether either of the callbacks has been used."""
     _data_callback: Callable[[], Counts]
     _truth_value_callback: Callable[[], bool]
 
@@ -32,29 +29,16 @@ class DieWithTruth(Die):
         """
         # Skip Die.__new__.
         self = super(Die, cls).__new__(cls)
-        self._used_callback = False
         self._data_callback = data_callback  # type: ignore
         self._truth_value_callback = truth_value_callback  # type: ignore
         return self
 
     @cached_property
     def _data(self):
-        if self._used_callback:
-            warnings.warn(
-                'Both the Die result and the truth value of a comparator were used. This is likely to be unintentional.',
-                category=RuntimeWarning,
-                stacklevel=3)
-        self._used_callback = True
         return self._data_callback()
 
     @cached_property
     def _truth_value(self):
-        if self._used_callback:
-            warnings.warn(
-                'Both the Die result and the truth value of a comparator were used. This is likely to be unintentional.',
-                category=RuntimeWarning,
-                stacklevel=4)
-        self._used_callback = True
         return self._truth_value_callback()
 
     def __bool__(self):
