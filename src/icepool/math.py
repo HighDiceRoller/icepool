2ae48e40ff75a9fc5e6b72df402a614cf9eb8b3c
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-06-17 20:27:31 -0700

    non-recursive cache for comb_row

--- src/icepool/math.py
+++ src/icepool/math.py
@@ -1,20 +1,25 @@
 __docformat__ = 'google'
 
-from functools import cache
+from collections.abc import MutableMapping
+
+# b -> list of rows
+comb_row_cache: MutableMapping[int, list[tuple[int, ...]]] = {}
 
 
-@cache
 def comb_row(n: int, b: int) -> tuple[int, ...]:
     """Returns a tuple of n+1 elements, where the kth element is equal to math.comb(n, k) * b ** k.
 
     The results are cached.
     """
-    if n == 0:
-        return (1,)
-    else:
-        prev = comb_row(n - 1, b)
-        return (1,) + tuple(
+    if b not in comb_row_cache:
+        comb_row_cache[b] = [(1,)]
+    rows = comb_row_cache[b]
+    while len(rows) < n + 1:
+        prev = rows[-1]
+        next = (1,) + tuple(
             x + b * y for x, y in zip(prev[1:], prev[:-1])) + (b * prev[-1],)
+        rows.append(next)
+    return rows[n]
 
 
 def comb(n: int, k: int, b: int = 1) -> int:
