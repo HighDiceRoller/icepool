467b94e94f94ea7c00fcaef3362cc661b6349d94
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-06-20 15:57:59 -0700

    cortex_prime.html: checkbox to count hitches as 1s

--- apps/common.js
+++ apps/common.js
@@ -4,15 +4,20 @@
 function validateInputs() {
     let changed = false;
     $("input").each(function() {
-        let originalVal = parseInt($(this).val());
-        let maxVal = parseInt($(this).attr('max'));
-        let minVal = parseInt($(this).attr('min'));
-        if (originalVal > maxVal) {
-            $(this).val(maxVal);
-            changed = true;
-        } else if (originalVal < minVal) {
-            $(this).val(minVal);
-            changed = true;
+        if ($(this).attr('type') == 'checkbox') {
+            let is_checked = $(this).attr('checked');
+            $(this).val(is_checked);
+        } else {
+            let originalVal = parseInt($(this).val());
+            let maxVal = parseInt($(this).attr('max'));
+            let minVal = parseInt($(this).attr('min'));
+            if (originalVal > maxVal) {
+                $(this).val(maxVal);
+                changed = true;
+            } else if (originalVal < minVal) {
+                $(this).val(minVal);
+                changed = true;
+            }
         }
     });
     
@@ -23,10 +28,14 @@ function validateInputs() {
 function setInputsFromSearchQuery() {
     let searchParams = new URLSearchParams(window.location.search);
     searchParams.forEach(function(value, key) {
-        let n = parseInt(value);
         let field = $("#" + key);
-        if (!(n >= field.attr('min') && n <= field.attr('max'))) {
-            return;
+        if (field.attr('type') == 'checkbox') {
+            field.attr('checked', true);
+        } else {
+            let n = parseInt(value);
+            if (!(n >= field.attr('min') && n <= field.attr('max'))) {
+                return;
+            }
         }
         field.val(value);
     });
