d5749aa95cfda4ece97f4d5bc273155c9c569335
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2022-12-25 23:51:56 -0800

    add missing typing.py

---
+++ src/icepool/typing.py
@@ -0,0 +1,13 @@
+from typing import Hashable, Protocol, TypeVar
+
+T_contra = TypeVar('T_contra', contravariant=True)
+
+
+class Outcome(Hashable, Protocol[T_contra]):
+    """Protocol to verify that outcomes are hashable and sortable."""
+
+    def __lt__(self, other: T_contra):
+        ...
+
+    def __eq__(self, other):
+        ...
