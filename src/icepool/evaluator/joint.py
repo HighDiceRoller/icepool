e848022b023fb3a98ba6923b4d3110ea61e390f7
Albert Julius Liu <ajul1987@gmail.com>
2023-01-07 21:26:07 -0800

    send Nones to inners of JointEvaluator if no outcomes were seen

--- src/icepool/evaluator/joint.py
+++ src/icepool/evaluator/joint.py
@@ -21,8 +21,8 @@ Q_contra = TypeVar('Q_contra', contravariant=True)
 class JointEvaluator(OutcomeCountEvaluator[T_contra, tuple, Q_contra]):
     """An `OutcomeCountEvaluator` that jointly evaluates sub-evaluators on the same roll(s) of a generator."""
 
-    def __init__(self, *sub_evaluators: OutcomeCountEvaluator):
-        self._sub_evaluators = sub_evaluators
+    def __init__(self, *inners: OutcomeCountEvaluator):
+        self._inners = inners
 
     def next_state(self, state, outcome, *counts):
         """Runs `next_state` for all subevals.
@@ -32,11 +32,11 @@ class JointEvaluator(OutcomeCountEvaluator[T_contra, tuple, Q_contra]):
         if state is None:
             return tuple(
                 evaluator.next_state(None, outcome, *counts)
-                for evaluator in self._sub_evaluators)
+                for evaluator in self._inners)
         else:
             return tuple(
                 evaluator.next_state(substate, outcome, *counts)
-                for evaluator, substate in zip(self._sub_evaluators, state))
+                for evaluator, substate in zip(self._inners, state))
 
     def final_outcome(self, final_state,
                       *generators: icepool.OutcomeCountGenerator):
@@ -44,9 +44,11 @@ class JointEvaluator(OutcomeCountEvaluator[T_contra, tuple, Q_contra]):
 
         The final outcome is a tuple of the final suboutcomes.
         """
+        if final_state is None:
+            final_state = (None,) * len(generators)
         return tuple(
-            evaluator.final_outcome(final_substate, *generators) for evaluator,
-            final_substate in zip(self._sub_evaluators, final_state))
+            inner.final_outcome(final_substate, *generators)
+            for inner, final_substate in zip(self._inners, final_state))
 
     def order(self, *generators: icepool.OutcomeCountGenerator):
         """Determines the common order of the subevals.
@@ -55,8 +57,7 @@ class JointEvaluator(OutcomeCountEvaluator[T_contra, tuple, Q_contra]):
             ValueError: If subevals have conflicting orders, i.e. some are
                 ascending and others are descending.
         """
-        suborders = tuple(
-            evaluator.order(*generators) for evaluator in self._sub_evaluators)
+        suborders = tuple(inner.order(*generators) for inner in self._inners)
         ascending = any(x > 0 for x in suborders)
         descending = any(x < 0 for x in suborders)
         if ascending and descending:
