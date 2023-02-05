8824d27c5344e0af61a0b89ed442404b1a703255
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2023-02-04 18:53:49 -0800

    remove fully-bound multiset function tests

--- tests/multiset_function_test.py
+++ tests/multiset_function_test.py
@@ -4,19 +4,10 @@ import pytest
 from icepool import multiset_function, d6, cartesian_product, MultisetExpression
 
 
-def test_fully_bound_expression():
-
-    @multiset_function
-    def evaluator():
-        return d6.pool(3).sum()
-
-    assert evaluator() == 3 @ d6
-
-
-def test_fully_bound_joint_expression():
+def test_single_expression():
 
     @multiset_function
     def evaluator(a):
-        return d6.pool(3).sum(), a.sum()
+        return a.sum()
 
-    assert evaluator(d6.pool(2)) == cartesian_product(3 @ d6, 2 @ d6)
+    assert evaluator(d6.pool(3)) == 3 @ d6
