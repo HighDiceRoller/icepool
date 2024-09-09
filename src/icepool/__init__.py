a33f3cc1e615bc250fd98bf29f94a4b7296b75c4
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2024-09-08 18:52:32 -0700

    remove doc line regarding zero quantities

--- src/icepool/__init__.py
+++ src/icepool/__init__.py
@@ -13,8 +13,6 @@ General conventions:
 * Instances are immutable (apart from internal caching). Anything that looks
     like it mutates an instance actually returns a separate instance with the
     change.
-* Unless explictly specified otherwise, elements with zero quantity, rolls, etc.
-    are considered.
 """
 
 __docformat__ = 'google'
