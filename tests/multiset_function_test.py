3b9781cf6436adc4cbb3cb0a21b8568d67c9dd25
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2024-12-26 23:58:33 -0800

    add simple test for bound generator

--- tests/multiset_function_test.py
+++ tests/multiset_function_test.py
@@ -11,3 +11,12 @@ def test_single_expression():
         return a.sum()
 
     assert evaluator(d6.pool(3)) == 3 @ d6
+
+
+def test_bound_generator():
+
+    @multiset_function
+    def evaluator(a):
+        return (a + d6.pool(2)).sum()
+
+    assert evaluator(d6.pool(1)) == 3 @ d6
