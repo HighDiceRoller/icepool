f17e2328500ee512680d6ef3cf279755a5063c1f
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2025-03-24 00:33:48 -0400

    update match expressions

--- src/icepool/operator/match.py
+++ src/icepool/operator/match.py
@@ -4,7 +4,7 @@ import icepool
 
 from icepool.expression.multiset_expression import MultisetExpression
 from icepool.operator.multiset_operator import MultisetOperator
-from icepool.order import Order, OrderReason
+from icepool.order import Order, OrderReason, UnsupportedOrderError
 
 import operator
 from abc import abstractmethod
@@ -17,15 +17,9 @@ from icepool.typing import T
 
 class MultisetSortMatch(MultisetOperator[T]):
 
-    def __init__(self,
-                 left: MultisetExpression[T],
-                 right: MultisetExpression[T],
-                 *,
-                 order: Order,
-                 tie: int,
-                 left_first: int,
-                 right_first: int,
-                 left_lead: int = 0):
+    def __init__(self, left: MultisetExpression[T],
+                 right: MultisetExpression[T], *, order: Order, tie: int,
+                 left_first: int, right_first: int):
         if order == Order.Any:
             order = Order.Descending
         self._children = (left, right)
@@ -33,87 +27,83 @@ class MultisetSortMatch(MultisetOperator[T]):
         self._tie = tie
         self._left_first = left_first
         self._right_first = right_first
-        self._left_lead = left_lead
 
-    def _copy(self, new_children: 'tuple[MultisetExpression[T], ...]'):
-        return MultisetSortMatch(*new_children,
-                                 order=self._order,
-                                 tie=self._tie,
-                                 left_first=self._left_first,
-                                 right_first=self._right_first,
-                                 left_lead=self._left_lead)
+    def _initial_state(self, order, outcomes) -> int:
+        """
+        
+        Returns:
+            left_lead: The number of elements by which the left operand is
+                ahead.
+        """
+        if order != self._order:
+            raise UnsupportedOrderError()
+        return 0
 
-    def _transform_next(self,
-                        new_children: 'tuple[MultisetExpression[T], ...]',
-                        outcome: T, counts: 'tuple[int, ...]'):
-        left_count, right_count = counts
+    def _next_state(self, left_lead, order, outcome, child_counts,
+                    source_counts, param_counts):
+        left_count, right_count = child_counts
         left_count = max(left_count, 0)
         right_count = max(right_count, 0)
 
         count = 0
-
-        if self._left_lead >= 0:
-            count += max(min(right_count - self._left_lead, left_count),
+        if left_lead >= 0:
+            count += max(min(right_count - left_lead, left_count),
                          0) * self._tie
-        elif self._left_lead < 0:
-            count += max(min(left_count + self._left_lead, right_count),
+        elif left_lead < 0:
+            count += max(min(left_count + left_lead, right_count),
                          0) * self._tie
-            count += min(-self._left_lead, left_count) * self._right_first
+            count += min(-left_lead, left_count) * self._right_first
 
-        next_left_lead = self._left_lead + left_count - right_count
+        left_lead += left_count - right_count
 
-        if next_left_lead > 0:
-            count += min(next_left_lead, left_count) * self._left_first
+        if left_lead > 0:
+            count += min(left_lead, left_count) * self._left_first
 
-        return MultisetSortMatch(*new_children,
-                                 order=self._order,
-                                 tie=self._tie,
-                                 left_first=self._left_first,
-                                 right_first=self._right_first,
-                                 left_lead=next_left_lead), count
+        return left_lead, count
+
+    @property
+    def _expression_key(self):
+        return MultisetSortMatch, self._order, self._tie, self._left_first, self._right_first
 
 
 class MultisetMaximumMatch(MultisetOperator[T]):
 
-    def __init__(self,
-                 left: MultisetExpression[T],
-                 right: MultisetExpression[T],
-                 *,
-                 order: Order,
-                 match_equal: bool,
-                 keep: bool,
-                 prev_matchable=0):
+    def __init__(self, left: MultisetExpression[T],
+                 right: MultisetExpression[T], *, order: Order,
+                 match_equal: bool, keep: bool):
         self._children = (left, right)
         self._order = order
         self._match_equal = match_equal
         self._keep = keep
-        self._prev_matchable = prev_matchable
 
-    def _copy(self, new_children: 'tuple[MultisetExpression[T], ...]'):
-        return MultisetMaximumMatch(*new_children,
-                                    order=self._order,
-                                    match_equal=self._match_equal,
-                                    keep=self._keep,
-                                    prev_matchable=self._prev_matchable)
+    def _initial_state(self, order, outcomes):
+        """
+        
+        Returns:
+            prev_matchable: The number of previously-seen elements that are
+                eligible to be matched.
+        """
+        if order != self._order:
+            raise UnsupportedOrderError()
+        return 0
 
-    def _transform_next(self,
-                        new_children: 'tuple[MultisetExpression[T], ...]',
-                        outcome: T, counts: 'tuple[int, ...]'):
-        left_count, right_count = counts
+    def _next_state(self, prev_matchable, order, outcome, child_counts,
+                    source_counts, param_counts):
+        left_count, right_count = child_counts
         left_count = max(left_count, 0)
         right_count = max(right_count, 0)
 
         if self._match_equal:
-            new_matches = min(self._prev_matchable + right_count, left_count)
+            new_matches = min(prev_matchable + right_count, left_count)
         else:
-            new_matches = min(self._prev_matchable, left_count)
-        next_prev_matchable = self._prev_matchable + right_count - new_matches
+            new_matches = min(prev_matchable, left_count)
+        prev_matchable += right_count - new_matches
         if self._keep:
             count = new_matches
         else:
             count = left_count - new_matches
-        return MultisetMaximumMatch(*new_children,
-                                    order=self._order,
-                                    match_equal=self._match_equal,
-                                    keep=self._keep,
-                                    prev_matchable=next_prev_matchable), count
+        return prev_matchable, count
+
+    @property
+    def _expression_key(self):
+        return MultisetMaximumMatch, self._order, self._match_equal, self._keep
