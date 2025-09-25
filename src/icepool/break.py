6b0ccf47f39e949978aa6dd94174a241b7955c44
Albert Julius Liu <ajul1987+highdiceroller@gmail.com>
2025-09-25 15:22:10 +0800

    start exploring `Break` wrapper

---
+++ src/icepool/break.py
@@ -0,0 +1,20 @@
+__docformat__ = 'google'
+
+from typing import Generic
+from icepool.typing import T
+
+
+class Break(Generic[T]):
+    """Wrapper around a return value for triggering an early exit from `map(repeat)`."""
+
+    def __init__(self, outcome: T | None = None):
+        """Constructor.
+        
+        Args:
+            outcome: The wrapped outcome. If not provided, the outcome is
+                considered to be equal to the input to the `map`ped function.
+        """
+        self.outcome = outcome
+
+    def __hash__(self) -> int:
+        return hash((Break, self.outcome))
